0d8ad80e190113f49a6866a19166ef5dd9d2e461
Nick Fisher <nick0fisher@gmail.com>
2019-06-28 17:54:51 -0700

    Refactor app to use new header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.scss';
+import Header from './Header';
 
-const App: React.FC = () => {
+export interface SiteModel {
+  title: string;
+  publicationInfo: string;
+  paragraphs: string[];
+}
+
+interface AppProps {
+  siteModel: SiteModel;
+}
+
+const App: FC<AppProps> = ({siteModel}) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1 children={siteModel.title} />
+      {siteModel.paragraphs.map(p => <p children={p} />)}
     </div>
   );
 }
