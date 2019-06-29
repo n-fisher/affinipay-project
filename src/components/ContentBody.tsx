be8993fd85f69268fcc57e5242166df7c862ecb7
Nick Fisher <nick0fisher@gmail.com>
2019-06-28 23:03:02 -0700

    Flesh out find-word-on-click with <textarea>s
    - Change spans to textareas to take advantage of HTML5 apis
    - Add function to dynamically resize textarea due to lack of style support
    - Add onclick/findWords functions that find and highlight a clicked word
    - Add some textarea specific styling for closer visual parity

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -5,20 +5,65 @@ interface ContentBodyProps {
   siteModel: SiteModel;
 }
 
-function generateParagraphs(siteModel: SiteModel): ReactElement[]  {
-  return siteModel.paragraphs.map((p, idx) => 
-    <p>
-      {idx === 0 &&
-      <span className='meta'>{siteModel.publicationInfo} - </span>}
-      {p}
-    </p>
-  );
+const findWordBoundsFromIndex = (string: string, index: number): number[] => {
+  // caveat: will not find words with special characters (ñ)
+  const isAlpha = (char: string) => char.match(/[a-z]/i);
+
+  // check if we have clicked an alphabetical character, and if there's any other contiguous ones
+  if (index < string.length && isAlpha(string[index])) {
+    let startingIndex = index;
+    while (startingIndex > 0 && isAlpha(string[startingIndex-1])) {
+      startingIndex -= 1;
+    }
+
+    let endingIndex = index;
+    while (endingIndex < string.length-1 && isAlpha(string[endingIndex+1])) {
+      endingIndex += 1;
+    }
+
+    return [startingIndex, endingIndex+1];
+  }
+
+  return [-1, -1];
 }
+
+function onClick(this: HTMLTextAreaElement): void {
+  const [start, end] = findWordBoundsFromIndex(this.value, this.selectionStart);
+  if (start !== -1 && end !== -1) this.setSelectionRange(start, end);
+}
+
+const resizeTextArea = (ref: HTMLTextAreaElement | null): void => {
+  // need to trigger size->0->newsize as text area will expand but not contract otherwise
+  const resize = () => {
+    if (!ref) return;
+    ref.style.height = '0px';
+    ref.style.height = ref.scrollHeight+'px';
+  };
+
+  // Add resize listener and trigger it for initial height setting
+  if (ref) {
+    const onclick = onClick.bind(ref);
+    ref.onclick = onclick;
+    window.addEventListener('resize', resize);
+    resize();
+  }
+}
+
+const generateParagraphs = (siteModel: SiteModel): ReactElement[] => (
+  siteModel.paragraphs.map((p, idx) => {
+    const text = (idx === 0 ? `${siteModel.publicationInfo} - ` : '') + p;
+    return <textarea readOnly ref={resizeTextArea} value={text} key={idx} />
+  })
+);
 
 const ContentBody: FC<ContentBodyProps> = ({siteModel}) => {
   return (
     <div className="content-body">
-      <h1 children={siteModel.title} />
+      <textarea className='title'
+        ref={resizeTextArea}
+        value={siteModel.title}
+        readOnly
+      />
       {generateParagraphs(siteModel)}
     </div>
   );
